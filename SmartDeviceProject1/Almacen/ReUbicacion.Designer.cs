461aed3ba03dbc96b9c0fdd540faf264ea8e33a1
morgadodeveloper19 <58676627+morgadodeveloper19@users.noreply.github.com>
2019-12-13 12:50:36 -0600

    reubicacion de material

---
+++ SmartDeviceProject1/Almacen/ReUbicacion.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SmartDeviceProject1.Almacen
+{
+    partial class Re_Ubicacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbCodigo = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbZonas = new System.Windows.Forms.ComboBox();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuItem1);
+            this.mainMenu1.MenuItems.Add(this.menuItem2);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.Text = "INGRESA EL TAG:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbCodigo
+            // 
+            this.lbCodigo.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold);
+            this.lbCodigo.ForeColor = System.Drawing.Color.Blue;
+            this.lbCodigo.Location = new System.Drawing.Point(4, 39);
+            this.lbCodigo.Name = "lbCodigo";
+            this.lbCodigo.Size = new System.Drawing.Size(233, 20);
+            this.lbCodigo.Text = "codigo";
+            this.lbCodigo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(4, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 52);
+            this.label2.Text = "descripcion";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(3, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 20);
+            this.label3.Text = "pzas";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // cbZonas
+            // 
+            this.cbZonas.Location = new System.Drawing.Point(28, 158);
+            this.cbZonas.Name = "cbZonas";
+            this.cbZonas.Size = new System.Drawing.Size(202, 22);
+            this.cbZonas.TabIndex = 5;
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Text = "CANCELAR";
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Text = "REUBICAR";
+            // 
+            // Re_Ubicacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.cbZonas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbCodigo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Menu = this.mainMenu1;
+            this.Name = "Re_Ubicacion";
+            this.Text = "REUBICACION";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbCodigo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.ComboBox cbZonas;
+    }
+}
---
+++ SmartDeviceProject1/Almacen/ReUbicacion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SmartDeviceProject1.Almacen
+{
+    public partial class Re_Ubicacion : Form
+    {
+        cMetodos cm = new cMetodos();
+
+        public Re_Ubicacion()
+        {
+            InitializeComponent();
+            string query = "SELECT Descripcion AS Items, Descripcion AS ID FROM ZonaBustamante";
+            llenaComboBox(cbZonas, "Items", "ID", query, cMetodos.CONEXION);
+        }
+
+        
+
+        public void llenaComboBox(ComboBox Objeto, string nomCve, string idCve, string consulta, string conex)
+        {
+            DataTable dt = cm.getDatasetConexionWDR(consulta, conex);
+            if (dt == null)
+            {
+                MessageBox.Show("NO SE PUEDE CONSULTAR LAS UBICACIONES EN ESTE MOMENTO", "ERROR");
+                this.Close();
+                return;
+            }
+
+            Objeto.DataSource = null;
+            Objeto.DataSource = dt;
+            Objeto.DisplayMember = nomCve;
+            Objeto.ValueMember = idCve;
+            dt.Columns[0].MaxLength = 255;
+            DataRow dr = dt.NewRow();
+            string opcSelec = "SELECCIONAR UBICACIÓN";
+            dr[nomCve] = (dt.Rows.Count > 0) ? opcSelec : "NO HAY UBICACIONES DISPONIBLES";
+            dr[idCve] = 0;
+            try
+            {
+                dt.Rows.InsertAt((dr), 0);
+            }
+            catch (Exception e)
+            {
+                dt.Columns[0].MaxLength = 255;
+                dt.Rows.InsertAt((dr), 0);
+                //throw;
+            }
+            Objeto.SelectedValue = 0;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+    }
+}
